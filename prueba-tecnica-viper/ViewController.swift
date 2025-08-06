cefd750f85b646729275a361ab84b363b52d288a
Luis Segoviano <the.segoviano@gmail.com>
2025-08-06 11:03:48 -0600

    Commit inicial

---
+++ prueba-tecnica-viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prueba-tecnica-viper
+//
+//  Created by Luis Segoviano on 06/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
